4868f1661c97ab992e49afa3bc0fda70f4d81f9e
Toby Warburton <tobywarburton10@gmail.com>
2023-08-10 19:26:58 -0700

    USE sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,6 @@
--- Drops the sample_db if it exists currently --
+-- Drops the company_db if it exists currently --
 DROP DATABASE IF EXISTS company_db;
--- Creates the sample_db database --
+-- Creates the company_db database --
 CREATE DATABASE company_db;
+-- Uses the company_db database
+USE company_db;
